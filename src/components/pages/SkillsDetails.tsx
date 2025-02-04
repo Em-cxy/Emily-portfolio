3e36ef6f5fc4f50328b479ad95b53b5d01d27b26
Aiyern30 <ianbian2@gmail.com>
2025-02-04 08:57:33 +0800

    enhance skills details part

--- src/components/pages/SkillsDetails.tsx
+++ src/components/pages/SkillsDetails.tsx
@@ -75,7 +75,7 @@ const SkillsDetails = () => {
         <div className="h-1 w-16 bg-white mx-auto text-center mt-4"></div>
       </div>
       {/* <Framework /> */}
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-10">
         {About.map((item) => (
           <div
             key={item.id}
