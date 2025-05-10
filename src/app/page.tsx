a22e2c39df3de2ab5c9ad6f5cdb19a3703b6d968
Em-cxy <emilychuxinyi88@gmail.com>
2025-05-10 14:20:08 +0800

    hrirjt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ import ContactForm from "@/components/contactform";
 
 // Mock data for all sections
 const profileData = {
-  name: "Emily",
+  name: "jedenmdkEmily",
   title: "Dance Instructor & Administrative Professional",
   description: "Hey, this is Chu Xin Yi. A showcase of my achievements, education, and professional experience!",
   image: "/Profile.jpg",
